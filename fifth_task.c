ff38f0bb4698cd9e73467a22f7e3f6fdac8489ad
ZKolos <79714184+ZKolos@users.noreply.github.com>
2025-11-13 16:49:27 +0000

    z

--- fifth_task.c
+++ fifth_task.c
@@ -9,9 +9,33 @@ containing hello, world!: the second string has been inserted between letters "o
 */
 
 #include <stdio.h> //don't forget to include the required libraries
+#include <string.h>
+#include <stdlib.h>
+
+char* insert(char*original, int position, char* text){
+  int lenog = strlen(original);
+  int lentext = strlen(text);
+  int len = lenog+lentext;
+  char*new=(char*)malloc(sizeof(char)*(len+1));
+  for (int i = 0; i < len; i++)
+  {
+    if (i<=position-1){
+      new[i]=original[i];
+    }else if(i<=(position+lentext)){
+      new[i]=text[i-position-1];
+    }else{
+      new[i]=original[i-1+lentext];
+    }
+  }
+  new[len]='\0';
+  return new;
+}
 
 int main()
 {
-
+  char string[101] = "This is a text not containing anything rude or harsh";
+  char *ins = insert(string, 2, "gurt sybau");
+  printf("%s", ins);
+  free(ins);
   return 0;
 }
