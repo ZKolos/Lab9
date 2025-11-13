afa1a86b9e87a659ec3aac9fdc0df8a08a1a79d0
ZKolos <79714184+ZKolos@users.noreply.github.com>
2025-11-13 16:37:20 +0000

    4

--- fourth_task.c
+++ fourth_task.c
@@ -19,8 +19,36 @@ Don't forget to release the memory at the end.
 #include <string.h>
 #include <stdlib.h>
 
+char* removestring(char* original, int start, int end)
+{
+  if(start < 0){
+    printf("Invalid starting value");
+    return NULL;
+  }
+  int originalLen=strlen(original);
+  if(end > originalLen){
+    printf("Invalid end value");
+    return NULL;
+  }
+  if (start >= end){
+    printf("Start has to be smaller than end");
+    return NULL;
+  }
+  int len = originalLen-(end-start);
+  char* new=(char*)malloc(sizeof(char)*(len+1));
+  for (int i = 0; i < len; i++)
+  {
+    new[i]=(i<start-1)?(original[i]):(original[end+i-start]);
+  }
+  new[len]='\0';
+  return new;
+}
+
 int main(){
-
-
+  char string[101] = "This is a text not containing anything rude or harsh"; // this is an array on the stack, mutable!
+  char *remstr = removestring(string, 2, 10);
+  printf("%s", remstr);
+  // Deallocate!!
+  free(remstr);
   return 0;
 }
